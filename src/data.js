4eb76486319a0b9255d4912b00ea62068c4fb3ec
edinakanlic <edina.kanlic@gmail.com>
2022-02-26 12:44:09 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+
+export default [
+  {
+    id: 1,
+    name: 'Elle Bobs',
+    age: 29,
+    image:
+      'https://images.pexels.com/photos/2219273/pexels-photo-2219273.jpeg?cs=srgb&dl=pexels-glauber-torquato-2219273.jpg&fm=jpg',
+  },
+  {
+    id: 2,
+    name: 'Jolly Jown',
+    age: 32,
+    image:
+      'https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    age: 36,
+    image:
+      'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  },
+  {
+    id: 4,
+    name: 'Lexy Gardner',
+    age: 34,
+    image:
+      'https://images.pexels.com/photos/2787341/pexels-photo-2787341.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  },
+  {
+    id: 5,
+    name: 'Steve Walsh ',
+    age: 29,
+    image:
+      'https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  },
+];
